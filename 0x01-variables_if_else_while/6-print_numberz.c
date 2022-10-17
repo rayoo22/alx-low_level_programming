73e1e442fe94ed3a514d6113dc5537e7e19af90d
rayoo2 <ryankibet14@gmail.com>
2022-10-17 05:53:51 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i = '0';
+
+for (i = '0'; i <= '9'; i++)
+	putchar(i);
+putchar('\n');
+return (0);
+}
