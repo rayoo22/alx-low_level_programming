2d08606feaa8a89e2b5d87f331490d518d4a523a
rayoo2 <ryankibet14@gmail.com>
2022-10-26 06:24:33 -0700

    arrays are not pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,5 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half a string
+ * @str: pointer
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_array - this is the main function
+ * @a: This is a pointer
+ * @n: this represents integer elements in an array
+ *
+ * Return: 0.
+ */
+
+void print_array(int *a, int n)
+{
+	for (n = 0; n <= strlen(array); n++)
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+
+	_putchar('\n');
+}
