6eb21ae1e9ab96dcd76dc831fb01d2925609ff92
rayoo2 <ryankibet14@gmail.com>
2022-11-01 12:55:17 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strspn - the main code.
+ * @s: this is a pointer.
+ * @accept: string containing list of characters.
+ *
+ * Return - number of bytes in the initial segment.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n;
+
+	n = strspn(s, accept);
+
+	return (n);
+}
