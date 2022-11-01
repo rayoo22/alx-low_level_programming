d3da7cf4390b07c58eb1352a451d8186ade496d4
rayoo2 <ryankibet14@gmail.com>
2022-11-01 12:05:03 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _memset - main function.
+ * @s: this is a pointer.
+ * @b: a characer.
+ * @n: This is the number of bytes to be set to the value.
+ *
+ * Return: 0.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, n);
+
+	return (0);
+}
