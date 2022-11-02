4c04562e664ac7aa317e7885a120797e46ce6e31
rayoo2 <ryankibet14@gmail.com>
2022-11-02 09:21:02 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - the main function.
+ * @int: empty parameter.
+ * Return: 0.
+ */
+int _abs(int)
+{
+	int j;
+	int i;
+
+	i = abs(j);
+	_putchar(i);
+
+	return (0);
+}
