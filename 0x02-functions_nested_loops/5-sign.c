8f8c6d4da965479f4163fb37fafe12fb6737c378
rayoo2 <ryankibet14@gmail.com>
2022-11-02 09:07:43 -0700

    print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,13 +15,13 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		_putchar(0);
+		_putchar('0');
 		return (0);
 	}
 	else if (n < 0)
 	{
-		_putchar(1);
-		return (-1);
+		_putchar('-');
+		return ('-1');
 	}
 	return (0);
 }
