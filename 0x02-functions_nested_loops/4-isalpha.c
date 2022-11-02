6884d38bd01d110b943fdb6d3b6afeb8ae75c0d0
rayoo2 <ryankibet14@gmail.com>
2022-11-02 08:53:52 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * isalpha - the main function.
+ * @c: a variable.
+ *
+ * Return: 1 if lowercase, 0 if otherwise.
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
