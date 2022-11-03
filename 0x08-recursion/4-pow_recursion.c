f14dfd957da0a5ef060db51ce4d06d70a0526437
rayoo2 <ryankibet14@gmail.com>
2022-11-03 10:24:04 -0700

    power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - the main function
+ * @x: a param
+ * @y: a param
+ * Return: -1 if y is lower than zero
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	return (x ^ y);
+}
