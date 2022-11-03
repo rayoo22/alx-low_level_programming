7f577300ac1180965b9efc07e88ba8b545fe75a6
rayoo2 <ryankibet14@gmail.com>
2022-11-03 11:26:42 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * _sqrt_recursion - the main function
+ * @n: function param
+ *
+ * Return: -1.
+ */
+int _sqrt_recursion(int n)
+{
+	m = (double) n;
+
+	if (n < 0)
+		return (-1);
+
+	if (n == 0 || n == 1)
+		return (1);
+
+	printf("%lf", sqrt(m));
+
+	return (0);
+}
