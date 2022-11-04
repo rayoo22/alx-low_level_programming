9170291e275172a9d1744dd42849e1bbcac18fc1
rayoo2 <ryankibet14@gmail.com>
2022-11-03 19:58:54 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,45 +1,22 @@
 #include "main.h"
 
-/**
- * _strlen_recursion - finds the length of a string
- * @s: pointer to string
- * Return: length of a string
- */
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
 
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	return (_strlen_recursion(s + 1) + 1);
-}
 /**
- * is_palind_recursive - checks if two chars of a string are equal
- * @s: string to be checked
- * @i: first index
- * @j: last index
- * Return: 1 if equal, else 0
+ * find_strlen - Returns the length of a string
+ * @s: The string to be measured.
+ * Return: The length of the string.
  */
-int is_palind_recursive(char *s, int i, int j)
+int find_strlen(char *s)
 {
-	if (i == j)
-		return (1);
-	if (i == j - 1)
-		return (s[i] != s[j]);
-	if (s[i] != s[j])
-		return (0);
-	return (is_palind_recursive(s, i + 1, j - 1));
-}
-/**
- * is_palindrome - checks if a string is a palindrome
- * @s: pointer to string
- * Return: 1 if string is palindrome else 0
- */
-int is_palindrome(char *s)
-{
-	int len;
+	int len = 0;
 
-	len = _strlen_recursion(s);
-	if (len == 0 || *s != s[len - 1])
-		return (0);
-	return (is_palind_recursive(s, 0, len - 1));
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+	return (len);
 }
