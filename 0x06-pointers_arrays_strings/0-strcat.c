86b89a60116fb75ae7ae3ec202a89a151cd845e4
rayoo2 <ryankibet14@gmail.com>
2022-11-04 00:41:31 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,27 +7,12 @@
  * @dest: a pointer
  * @src: a pointer
  *
- * Description: This function should print the concatenation of two strings.
  * Return: a pointer to the resulting string dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	strcat(dest, src);
 
-	i = 0;
-	
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	j = 0;
-	while (src[j] != '\0')
-		dest[i] = src[j];
-		i++;
-		j++;
-	
-	dest[i] = '\0';
 	return (dest);
 }
