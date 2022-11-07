5abf2b14a797fb6bce10b16e6fdc08f98868cb26
rayoo2 <ryankibet14@gmail.com>
2022-11-07 01:24:46 -0800

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: the number of compiled arguments
+ * @argv: array of arguments.
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
