582943ae7813edb6e4ef6b5e4a1b775e3bc6d320
rayoo22 <ryankibet14@gmail.com>
2022-11-16 10:33:31 -0800

    pi

---
+++ 0x0D-preprocessor/1-main.c
@@ -0,0 +1,18 @@
+#include "1-pi.h"
+#include "1-pi.h"
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: 0
+ */
+int main(void)
+{
+	float a;
+	float r;
+
+	r = 98;
+	a = PI * r * r;
+	printf("%.3f\n", a);
+	return (0);
+}
