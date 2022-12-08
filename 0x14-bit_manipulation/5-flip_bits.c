3365c0bbbed05b4913121ecdfac2ec92084dbf1c
rayoo22 <ryankibet14@gmail.com>
2022-12-08 02:36:40 -0800

    tasks

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * flip_bits - counts the number of bits to change to get from one number to another
+ * flip_bits - counts the number of bits to change
  * @n: first member
  * @m: seconde member
  * Return: number of bits to change
