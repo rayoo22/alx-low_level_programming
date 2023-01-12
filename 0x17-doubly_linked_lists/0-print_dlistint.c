8c230fd51f32f2873eb5af9b1605784532f3c03e
rayoo22 <ryankibet14@gmail.com>
2023-01-12 02:46:57 -0800

    Print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints number of elements
+ * @h: a variable
+ * Return: number of nodes in list
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+	const dlistint_t *tmp = h;
+
+	while(tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+		i++;
+	}
+	return (i);
+}
