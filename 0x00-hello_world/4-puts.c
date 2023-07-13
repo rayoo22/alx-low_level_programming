7035b182ec7e08e556fe1440e458bc6f2841a42c
rayoo22 <ryankibet14@gmail.com>
2023-07-13 12:33:54 -0700

    printing statement using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * Return: 0. prints a sentence
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingial puzzle");
+	return (0);
+}
