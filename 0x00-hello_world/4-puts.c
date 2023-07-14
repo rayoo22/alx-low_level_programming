e62fb72f39ec7f590cef14c037574f0f5f98f1ba
rayoo22 <ryankibet14@gmail.com>
2023-07-13 19:19:29 -0700

    printing statement

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /**
  * main - Entry point.
- * Return: 0. prints a sentence
+ *
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingial puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
