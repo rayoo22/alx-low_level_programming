26465918500d3575145215004a5c31388d5bdc53
rayoo22 <ryankibet14@gmail.com>
2023-07-14 19:25:25 -0700

    prints letters except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints lettersexcept e and q
+ * Return:0\
+ */
+int main(void)
+{
+	int lower = 97;
+
+	while(lower < 123)
+	{
+		if (lower != 101 && lower != 113)
+		{
+			putchar(lower);
+		}
+		lower++;
+	}
+	putchar(10);
+
+	return (0);
+}
