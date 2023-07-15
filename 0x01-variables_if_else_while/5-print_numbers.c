49d05a32dc38035d54c1b64efb52d96be31d116e
rayoo22 <ryankibet14@gmail.com>
2023-07-14 19:29:27 -0700

    prints single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints single digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+	
+	num = 48;
+
+	while(num < 58)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar(10);
+
+	return(0);
+}
