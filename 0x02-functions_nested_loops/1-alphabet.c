b6772711846d51672dcd33fd05ade2ae0b0e1f0d
rayoo22 <ryankibet14@gmail.com>
2023-07-18 10:21:06 -0700

    printing _ p u t c h a r

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: prints the alphabet in lowercase, then new line
+ * Return: 0
+ */
+int main(void)
+{
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar("\n");
+
+	return (0);
+}
