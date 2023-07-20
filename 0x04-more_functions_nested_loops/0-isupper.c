a5cd5f0c4d6a7963edaea11b14f838edf14dc001
rayoo22 <ryankibet14@gmail.com>
2023-07-20 01:38:20 -0700

    check for uppercase letters

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - entry point
+ * Description: checks for uppercaase character
+ * Return: 1 if uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	return (0);
+}
