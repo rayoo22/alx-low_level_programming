e8b0ad74e968cba42bdd5ecad99c3051d35f1ab3
rayoo22 <ryankibet14@gmail.com>
2023-07-20 18:57:46 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,7 +5,36 @@
  */
 int main(void)
 {
-	char i;
+	int i;
 
-	for (i = '1'; i
+	while (i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			if (i == 100)
+			{
+				printf("Buzz");
+				printf("\n");
+			}
+			else
+			{
+				printf("Buzz");
+			}
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fuzz ");
+		}
+
+		else
+		{
+			printf("%d", h);
+		}
+		i++;
+	}
+	return (0);
 }
