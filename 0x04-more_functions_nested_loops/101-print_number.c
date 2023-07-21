b3a5605b50087b2f1a3787c0834be288f39b36ad
rayoo22 <ryankibet14@gmail.com>
2023-07-20 19:02:12 -0700

    prints an integer - recursion

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,5 +7,22 @@
  */
 void print_number(int n)
 {
-	_putchar(n + '0');
+	unsigned int x;
+
+	if (n < 0)
+	{
+		x = -n;
+		_putchar('-');
+	}
+	else
+	{
+		x = n;
+	}
+
+	if (x / 10)
+	{
+		print_number(x / 10);
+	}
+
+	_putchar((x % 10) + '0');
 }
