5ea7f7600880991d246bc2cf19bdf08ed925b91c
rayoo22 <ryankibet14@gmail.com>
2023-07-24 03:08:48 -0700

    prints a string, in reverse, followed by a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_rev - main entry point
+ * @s: variable in use
+ * Description: prints a string, in reverse, followed by a new line
+ */
+void print_rev(char *s)
+{
+	strrev(s);
+}
