dee5a16dc3851130621105763e70c32510447f8a
rayoo22 <ryankibet14@gmail.com>
2023-07-24 02:42:17 -0700

    prints a string, followed by a new line, to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _puts - main entry point
+ * @str: variable in use
+ * Description: prints a string, followed by a new line, to stdout
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
