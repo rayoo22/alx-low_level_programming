a95df06a06b07b431ece95a8ab9bbd910f95959e
rayoo22 <ryankibet14@gmail.com>
2023-07-24 11:10:13 -0700

    swaps the values of two integers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * puts_half - main entry point
+ * @str: variable in use
+ * Description: prints half of a string, followed by a new line
+ */
+void puts_half(char *str)
+{
+	int len;
+
+
+}
