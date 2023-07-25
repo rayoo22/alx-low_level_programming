502cbee9ea1cd204f18c9470b80cc9bc1a3a6959
rayoo22 <ryankibet14@gmail.com>
2023-07-24 19:19:46 -0700

    function should print only one character out of two

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,6 +7,26 @@
 void puts2(char *str)
 {
 	int i;
+	int t;
+	char *y;
+	int j;
 
-	for (i = 0; i < strlen(str); 
+	i = 0;
+	t = 0;
+	*y = str;
+
+	while (*y != '\0')
+	{
+		y++;
+		i++;
+	}
+	t = i - 1;
+	for (j = 0; j <= t; j++)
+	{
+		if (j % 2 == 0)
+		{
+			_putchar(str[j]);
+		}
+	}
+	_putchar('\n');
 }
