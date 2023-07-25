3884738e990f5cf55af00bfd54e4de98cdcdcb28
rayoo22 <ryankibet14@gmail.com>
2023-07-24 19:09:42 -0700

    prints a string in reverse, folloed by new line

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,5 +6,21 @@
  */
 void print_rev(char *s)
 {
-	strrev(s);
+	int i;
+	int j;
+
+	i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++
+	}
+	s--;
+	for (j = i; j > 0; j--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_array - main entry point
+ * @a: variable in use
+ * @n: variable in use
+ * Description:  prints n elements of an array of integers
+ * Return: 0
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	while (i < len(n - 1))
+	{
+		printf(
+	}
+}
