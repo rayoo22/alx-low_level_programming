37d114eff7b56cf90f5f04ed0896eb99e21a0696
rayoo22 <ryankibet14@gmail.com>
2023-07-27 04:35:44 -0700

    copies a string

---
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code 
+ * Return: 0
+ */
+int main(void)
+{
+	char s1[98];
+	char *ptr;
+	int i;
+
+
+    for (i = 0; i < 98 - 1; i++)
+    {
+	    s1[i] = '*';
+
+    }
+}
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,5 +9,5 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	strncpy(dest, src, n);
+	return (strncpy(dest, src, n));
 }
