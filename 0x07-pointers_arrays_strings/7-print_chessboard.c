16eee94b8117000d4404e45b3ea149b38fceda39
rayoo22 <ryankibet14@gmail.com>
2023-07-31 19:48:40 -0700

    prints the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,14 +4,15 @@
  * @a: character pointer of array size 8
  * Return: 0
  */
+
 void print_chessboard(char (*a)[8])
 {
 	int i; 
 	int j;
 
-	for (i = 1; i <= 8; i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (j = 1; j <= 8; j++)
+		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
 		}
