10d725bc7338bdfa6564cdf025653ebb875560e5
rayoo22 <ryankibet14@gmail.com>
2023-08-14 11:18:55 -0700

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog structure
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
 struct dog{
 	char * name;
 	float age;
