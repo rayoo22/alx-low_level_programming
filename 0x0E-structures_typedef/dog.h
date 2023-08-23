a1c6fa807ed9642058674c0a1c4677d6ea98047c
rayoo22 <ryankibet14@gmail.com>
2023-08-23 02:44:00 -0700

    creating a library

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,32 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * struct dog - dog structure
- * @name: first member
- * @age: second member
- * @owner: third member
- * Description: a structure of a dog
+ * struct dog - it gives info of a dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
  */
-struct dog{
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
-
-/**
- * dot_t - typedef for struct dog
- */
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-char *strcpy(char *dest, char *src);
-int _strlen(char *s);
 
 #endif
