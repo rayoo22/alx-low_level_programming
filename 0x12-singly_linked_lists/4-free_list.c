962202bfbc2b3e9b8b3286d28a9e7620bb41413c
rayoo22 <ryankibet14@gmail.com>
2023-08-24 09:59:37 -0700

    frees a linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - free a linked llist
+ * @head: the node to be freed
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
