fe3d997a1ac027289540ad08560fac6f1dab3614
rayoo22 <ryankibet14@gmail.com>
2023-08-24 10:25:32 -0700

    lists number of elements

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,7 +2,7 @@
 
 /**
  * list_len - returns the number of elements in a linked list
- * @n: pointer to the list_t list
+ * @h: pointer to the list_t list
  * Return: number of elements in the linked list
  */
 size_t list_len(const list_t *h)
