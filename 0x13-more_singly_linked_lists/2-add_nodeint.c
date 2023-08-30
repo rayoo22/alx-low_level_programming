5d9a50b80545bbf99f7e62bd70e2f612527900ef
rayoo22 <ryankibet14@gmail.com>
2023-08-29 23:53:45 -0700

    adding a new node to the linked list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,12 +8,6 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
-	unsigned int len = 0;
-
-	while (n[len])
-	{
-		len++;
-	}
 
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
@@ -22,9 +16,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	}
 
 	new_node->n = strdup(n);
-	new_node->len = len;
 	new_node->next = (*head);
 	(*head) = new_node;
 
-	return (*head);
+	return (new_node);
 }
