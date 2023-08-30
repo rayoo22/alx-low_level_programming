1c5153810fa7f5daa347ec1d23945a175cf6937e
rayoo22 <ryankibet14@gmail.com>
2023-08-29 17:39:08 -0700

    sum of data in nodes of a linked list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_listint - calculates the sum of data stored in nodes
+ * @head: head node of te linked list
+ * Return: total sum of data
+ */
+int sum_listint(listint_t *head)
+{
+	int total = 0;
+	listint_t *t = head;
+
+	while (temp)
+	{
+		total += t->n;
+		t = t->next;
+	}
+
+	return (total);
+}
