3380f3fdff54064dffde503ff15fe9ef10d82714
rayoo22 <ryankibet14@gmail.com>
2023-09-27 11:32:56 -0700

    raising to the power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - return the value of x to the power of y
+ * @x: value to find its result after power
+ * @y: value to raise on x
+ * Return: the value of x raised to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	double result;
+
+	result = pow(double(x), double(y));
+
+	return (result);
+}
