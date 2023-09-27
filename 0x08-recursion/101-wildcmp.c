ed5b851d1ddbe2d3a2a554a1dfddf6fb7fe74aa4
rayoo22 <ryankibet14@gmail.com>
2023-09-27 15:25:17 -0700

    comparing strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,23 +7,35 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0')
-	{
-		if (*s2 != '\0' && *s2 == '*')
-		{
-			return (wildcmp(s1, s2 + 1));
-		}
-		return (*s2 == '\0');
-	}
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	else if (*s2 == '*')
+		return (substring_match(s1, (s2 + 1), (s2 + 1)));
+	else
+		return (0);
+}
 
+/**
+ * substring_match - check if a substring after wildcard matches s1
+ * @s1: one string
+ * @s2: another tring
+ * @after_wldcd: position tight a fter wildcard
+ * Return: 1 if matched, 0 if not
+ */
+int substring_match(char *s1, char *s2, char *after_wldcd)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == '\0' && *s2 == '*')
+		return (substring_match(s1, s2 + 1, s2 + 1));
+	if (*s1 == '\0' && *s2 != '\0')
+		return (0);
 	if (*s2 == '*')
-	{
-		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-	}
-	else if (*s1 == *s2)
-	{
-		return (wildcmp((s1 + 1, s2 + 1));
-	}
-
-	return (0);
+		return (substring_match(s1, s2 + 1, s2 + 1));
+	if (*s1 == *s2)
+		return (substring_match(s1 + 1, s2 + 1, after_wldcd));
+	else
+		return (substring_match(s1 + 1, after_wldcd, after_wldcd
 }
