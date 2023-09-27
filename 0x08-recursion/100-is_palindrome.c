1abe1615dfc1056645e661625af47684de45a439
rayoo22 <ryankibet14@gmail.com>
2023-09-27 14:52:41 -0700

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+int find_strlen(char *s);
+int check_palindrome(char *s, int len, int index);
+int is_palindrome(char *s);
+
+/**
+ * find_strlen - returns the length of a string
+ * @s: the string to be measured.
+ * Return: the length of the string
+ */
+int find_dtrlen(char *s)
+{
+	int len = 0;
+
+	if (*(s + len))
+	{
+		len++;
+		len += find_strlen(s + len);
+	}
+
+	return (len);
+}
