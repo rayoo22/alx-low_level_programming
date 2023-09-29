38f4e1eb98266235949fa641b509bd1d5536bc7a
rayoo22 <ryankibet14@gmail.com>
2023-09-28 18:23:37 -0700

    comparing strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,21 +1,4 @@
 #include "main.h"
-/**
- * wildcmp - compares strings
- * @s1: pointer to a string
- * @s2: pointer to a string
- * Return: 0
- */
-int wildcmp(char *s1, char *s2)
-{
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-	else if (*s2 == '*')
-		return (substring_match(s1, (s2 + 1), (s2 + 1)));
-	else
-		return (0);
-}
 
 /**
  * substring_match - check if a substring after wildcard matches s1
@@ -37,5 +20,23 @@ int substring_match(char *s1, char *s2, char *after_wldcd)
 	if (*s1 == *s2)
 		return (substring_match(s1 + 1, s2 + 1, after_wldcd));
 	else
-		return (substring_match(s1 + 1, after_wldcd, after_wldcd
+		return (substring_match(s1 + 1, after_wldcd, after_wldcd));
+}
+
+/**
+ * wildcmp - compares strings
+ * @s1: one string
+ * @s2: another string
+ * Return: 1 if it matches, 0 if no match
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	else if (*s2 == '*')
+		return (substring_match(s1, (s2 + 1), (s2 + 1)));
+	else
+		return (0);
 }
