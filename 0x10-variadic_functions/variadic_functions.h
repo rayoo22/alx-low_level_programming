9fae1e63ce6da6db921751ef441e318ab0bb86eb
rayoo22 <ryankibet14@gmail.com>
2023-10-18 19:01:31 +0300

    prints all (int, char, float ...every datatype)

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,17 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * struct dt - struct
+ * @letter: letter corresponding to data type
+ * @func: function pointer
+*/
+typedef struct dt
+{
+char letter;
+void (*func)(va_list);
+} datatype;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
