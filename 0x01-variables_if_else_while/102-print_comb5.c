4863a46d5de3769b95267ee5668446cea82e74e7
rayoo22 <ryankibet14@gmail.com>
2023-10-18 20:31:16 +0300

    prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,39 +7,32 @@
  */
 int main(void)
 {
-	int i, j, k, m;
-	
-	i = 48
-	while (i < 58)
-	{
-		j = 48;
-		while (j < 58)
-		{
-			m = j + 1;
-			k = i;
-			while (k < 58)
-			{
-				while (m < 58)
-				{
-					putchar(i);
-					putchar(j);
-					putchar(32);
-					putchar(k);
-					putchar(m);
-					if (i < 57 || j < 56 || k < 57 ||m < 57)
-					{
-						putchar(44);
-						putchar(32);
-					}
-					m++;
-				}
-				m = 48;
-				k++;
-			}
-			j++;
-		}
-		i++;
-	}
-	putchar(10);
-	return (0);
+int tens, ones, t, o;
+
+for (tens = '0'; tens <= '9'; tens++)
+{
+for (ones = '0'; ones <= '9'; ones++)
+{
+for (t = tens; t <= '9'; t++)
+{
+for (o = ones + 1; o <= '9'; o++)
+{
+putchar(tens);
+putchar(ones);
+putchar(' ');
+putchar(t);
+putchar(o);
+
+if (!((tens == '9' && ones == '8') && (t == '9' && o == '9')))
+{
+putchar(',');
+putchar(' ');
+}
+}
+o = '0';
+}
+}
+}
+putchar('\n');
+return (0);
 }
