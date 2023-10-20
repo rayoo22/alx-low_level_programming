75dd0b19973b23af9be210628829f30fd9e1a566
rayoo22 <ryankibet14@gmail.com>
2023-10-20 05:10:07 +0300

    executes before main() function

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,10 +8,11 @@ size_t list_len(const list_t *h)
 {
 size_t count = 0;
 
-while (h != NULL) /* if a node does not exist */
+while (h != NULL)/* if a node does not exist */
 {
 count++;
 h = h->next;
 }
+
 return (count);
 }
---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * execute_before_main - executes before the main function
+ * Note: made possible by declaring "__attribute__ ((constructor))"
+*/
+void __attribute__ ((constructor)) execute_before_main()
+{
+printf("You're beat! and yet, you must allow,");
+printf("I bore my house upon my back!\n");
+printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)");
+}
