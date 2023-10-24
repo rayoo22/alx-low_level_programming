5a4310214be753dd3589ed39b0443b19d001d34b
rayoo22 <ryankibet14@gmail.com>
2023-10-24 03:10:54 +0300

    inserts a new node at a given index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,42 +8,38 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-unsigned int i = 0;
-listint_t *new_node, *tmp;
+unsigned int i;
+listint_t *new;
+listint_t *temp = *head;
 
-if (head == NULL)
+new = malloc(sizeof(listint_t));
+if (!new || !head)
 {
 return (NULL);
 }
 
-new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
-{
-return (NULL);
-}
-new_node->n = n;
+new->n = n;
+new->next = NULL;
 
 if (idx == 0)
 {
-new_node->next = *head;
-*head = new_node;
-return (new_node);
+new->next = *head;
+*head = new;
+return (new);
 }
 
-tmp = *head;
-while (i < (idx - 1))
+for (i = 0; temp && i < idx; i++)
 {
-tmp = tmp->next;
-i++;
-
-if (tmp == NULL)
+if (i == idx - 1)
 {
-free(new_node);
-return (NULL);
+new->next = temp->next;
+temp->next = new;
+return (new);
+}
+else
+{
+temp = temp->next;
 }
 }
-
-new_node->next = tmp->next;
-tmp->next = new_node;
-return (new_node);
+return  (NULL);
 }
