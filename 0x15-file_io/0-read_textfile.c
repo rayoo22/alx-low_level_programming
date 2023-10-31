edf54a0364dddf7fb4d7e9db3ab3509529432de0
rayoo22 <ryankibet14@gmail.com>
2023-10-31 04:59:24 +0300

    reads text file print to STDOUT

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,43 +7,23 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-ssize_t n_read, n_wrote;
-char *buffer;
-
-if (filename == NULL)
-{
-return (0);
-}
+char *buf;
+ssize_t fd;
+ssize_t w;
+ssize_t t;
 
 fd = open(filename, O_RDONLY);
+
 if (fd == -1)
 {
 return (0);
 }
 
-buffer = malloc(sizeof(char) * letters);
-if (buffer == NULL)
-{
-returns (0);
-}
+buf = malloc(sizeof(char) * letters);
+t = read(fd, buf, letters);
+w = write(STDOUT_FILENO, buf, t);
 
-n_read = read(fd, buffer, letters);
-if (n_read == -1)
-{
-free(buffer);
+free(buf);
 close(fd);
-return (0);
-}
-
-n_wrote = write(STDOUT_FILENO, buffer, n_read);
-if (n_wrote == -1)
-{
-free(buffer);
-close(fd);
-return (0);
-}
-
-close(fd);
-return (n_read);
+return (w);
 }
