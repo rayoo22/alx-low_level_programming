141ab5224c00f98b1e8d8aaf8b322d53f9dbdb62
rayoo22 <ryankibet14@gmail.com>
2023-10-31 05:05:46 +0300

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,20 +1,4 @@
 #include "main.h"
-/**
- * _strlen - find length of string
- * @str: string
- * Return: length
-*/
-int _strlen(char *str)
-{
-int len;
-
-for (len = 0; strlen[len] != '\0'; len++)
-{
-;
-}
-
-return (len);
-}
 
 /**
  * create_file - creats file with permissions
@@ -24,33 +8,29 @@ return (len);
 */
 int create_file(const char *filename, char *text_content)
 {
-int fd;
-int n_wrote;
+int fd, w, len = 0;
 
-if (!filename)
+if (filename == NULL)
+{
+return (-1);
+}
+if (text_content != NULL)
+{
+for (len = 0; text_content[len];)
+{
+len++;
+}
+}
+
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+w = write(fd, text_content, len);
+
+if (fd == -1 || w == -1)
 {
 return (-1);
 }
 
-fd = open(filename, 0_CREAT | 0_WRONLY | 0_TRUNC, 0600);
-if (fd == -1)
-{
-return (-1);
-}
-
-if (!text_content)
-{
 close(fd);
-return (1);
-}
 
-n_wrote = write(fd, text_content, _strlen(text_content));
-if (n_wrote == -1 || n_wrote != _strken(text_content))
-{
-close(fd);
-return (-1);
-}
-
-close(fd);
-return (1);
+return (1)
 }
