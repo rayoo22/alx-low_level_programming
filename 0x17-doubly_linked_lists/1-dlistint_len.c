b0823767dff370e3a4d786bff4435bb747a81317
rayoo22 <ryankibet14@gmail.com>
2023-12-07 09:50:47 -0800

    returns no. of elements in list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib>
+
+/**
+ * dlistint_len - returns the number of elements in list
+ * @h: the head node of the list
+ * Return: number of elements in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count;
+	dlistint_t *ptr;
+	
+	count = 0;
+	ptr = h;
+
+	if (ptr != NULL)
+	{
+		ptr = ptr->next;
+		count++;
+	}
+	return (count);
+}
