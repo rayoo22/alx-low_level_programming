c76b363f6c1ff037fd676ff1354b0848bdfef45d
rayoo22 <ryankibet14@gmail.com>
2023-12-28 04:00:00 -0800

    create a hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-/**
+/*
  * struct hash_node_s - Node of a hash table
  *
  * @key: The key, string
@@ -41,6 +41,39 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+
+/**
+ * struct shash_node_s - node of a sorted hash table
+ * @key: the key
+ * @value: the value corresponding
+ * @next: a pointer to the next node
+ * @sprev: a pointer to the previous element of the sorted linked list
+ * @snext: a pointer to the next element of the sorted linked list
+ */
+typedef struct shash_node_s
+{
+	char *key;
+	char *value;
+	struct shash_node_s *next;
+	struct shash_node_s *sprev;
+	struct shash_node_s *snext;
+} shash_node_t;
+
+/**
+ * struct shash_table_s - sorted hash table data structure
+ * @size: the size of the array
+ * @array: an array of size (@size)
+ * @shead: a pointer to the first element of the sorted linked list
+ * @stail: a pointer to the last element of the sorted linked list
+ */
+typedef struct shash_table_s
+{
+	unsigned long int size;
+	shash_node_t **array;
+	shash_node_t *shead;
+	shash_node_t *stail;
+} shash_table_t;
+
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
