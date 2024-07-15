0859bed09b926686c1802db170933517f58a6027
rayoo22 <ryankibet14@gmail.com>
2024-07-15 12:41:20 +0300

    binary search

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for element in a linear manner
+ * @array: pointer to the first element of the arrayto search in
+ * @size: number of elements in array
+ * @value: the value to search for
+ * Description: if value is not in array OR array is NULL, function must return -1
+ * Return: -1
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		if (value == array[i])
+		{
+			return i;
+		}
+	}
+	/* x is not found */
+	return -i;	
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,38 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for an element using binary search
+ * @array: pointer to the first element of the array
+ * @size: is the nu,ber of elements in array
+ * @value: is the value to search for
+ * Description: uses binary search to determine value to be searched
+ * Return: -1 if value is not in array
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t mid;
+	size_t start;
+	size_t end;
+
+	start = 0;
+	end = size - 1;
+
+	while (start <= end)
+	{
+		mid = floor(start + end) / 2;
+
+		if (array[mid] < value)
+		{
+			start = mid + 1;
+		}
+		else if (array[mid] > value)
+		{
+			end = mid - 1;
+		}
+		else 
+		{
+			return mid;
+		}
+	}
+	return(-1);
+}
